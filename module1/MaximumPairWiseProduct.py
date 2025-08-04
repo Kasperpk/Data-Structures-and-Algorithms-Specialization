c2e0fdc3aaaae5b27c4cba1309e7dc527f7df8b9
Kasperpk <135698388+Kasperpk@users.noreply.github.com>
2025-08-04 15:48:57 +0200

    Update MaximumPairWiseProduct.py

--- module1/MaximumPairWiseProduct.py
+++ module1/MaximumPairWiseProduct.py
@@ -1,1 +1,22 @@
+def MaximumPairWise(n,numbers):
+    '''
+    Adds two numbers
+    '''
+    if n >= 2 and n <= 200000:
+        current_number = 0
+        for n in numbers:
+            if n > current_number:
+                current_number = n
+        first_num_index = numbers.index(current_number)
 
+        current_number = 0
+        for n in numbers:
+            if numbers.index(n) != first_num_index and n > current_number:
+                current_number = n
+        
+        second_num_index = numbers.index(current_number)
+
+        return numbers[first_num_index] * numbers[second_num_index]
+    
+    else:
+        print("Input list is not within valid range")
