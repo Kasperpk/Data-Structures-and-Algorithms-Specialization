4a0937d92963a6f7ddab83cf5674db0733ae4c17
Kasperpk <135698388+Kasperpk@users.noreply.github.com>
2025-10-21 21:19:57 +0200

    Refactor binary tree traversal functions
    Refactor tree traversal functions to handle None nodes and improve clarity.

--- data-structures/module 5/problems/BinaryTreeTraversals.py
+++ data-structures/module 5/problems/BinaryTreeTraversals.py
@@ -1,81 +1,95 @@
 class Node:
-
     def __init__(self, key):
-
         self.key = key
         self.left = None
         self.right = None
 
-def in_order_traversal(tree,root,res):
+def in_order_traversal(tree, root, res):
     '''
     Input: 
-        r the root node of the tree
-        tree the list of nodes making up the tree
-    Output the keys in-order traversal
+        root: the current node being processed
+        tree: the list of nodes making up the tree
+        res: list to store the traversal result
+    Output: the keys in in-order traversal
     '''
-    if root.left == -1 and root.right == -1:
-        if root.key not in res:
-            res.append(root.key)
-        return 
+    if root is None:
+        return res
     
-
-    in_order_traversal(tree, tree[root.left],res)
-    if root.key not in res:
-        res.append(root.key)
-    in_order_traversal(tree, tree[root.right],res)
-    if root.key not in res:
-        res.append(root.key)
+    # Process left subtree
+    if root.left != -1:
+        in_order_traversal(tree, tree[root.left], res)
+    
+    # Process current node
+    res.append(root.key)
+    
+    # Process right subtree
+    if root.right != -1:
+        in_order_traversal(tree, tree[root.right], res)
+    
     return res
 
-def pre_order_traversal(tree, root,res):
-
-
-    if root.left == -1 and root.right == -1:
-        if root.key not in res:
-            res.append(root.key)
-        return 
-
+def pre_order_traversal(tree, root, res):
+    '''
+    Input: 
+        root: the current node being processed
+        tree: the list of nodes making up the tree
+        res: list to store the traversal result
+    Output: the keys in pre-order traversal
+    '''
+    if root is None:
+        return res
     
+    # Process current node
     res.append(root.key)
-    pre_order_traversal(tree, tree[root.left], res)
     
-    pre_order_traversal(tree, tree[root.right],res)
-
+    # Process left subtree
+    if root.left != -1:
+        pre_order_traversal(tree, tree[root.left], res)
+    
+    # Process right subtree
+    if root.right != -1:
+        pre_order_traversal(tree, tree[root.right], res)
+    
     return res
 
 def post_order_traversal(tree, root, res):
-
-    if root.left == -1 and root.right == -1:
-        if root.key not in res:
-            res.append(root.key)
-        return
+    '''
+    Input: 
+        root: the current node being processed
+        tree: the list of nodes making up the tree
+        res: list to store the traversal result
+    Output: the keys in post-order traversal
+    '''
+    if root is None:
+        return res
     
-    post_order_traversal(tree, tree[root.left], res)
-    post_order_traversal(tree, tree[root.right], res)
-
+    # Process left subtree
+    if root.left != -1:
+        post_order_traversal(tree, tree[root.left], res)
+    
+    # Process right subtree
+    if root.right != -1:
+        post_order_traversal(tree, tree[root.right], res)
+    
+    # Process current node
     res.append(root.key)
-
+    
     return res
 
-
-
 if __name__ == '__main__':
-    
+    # Read the number of nodes
     n = int(input())
-    lines = [list(map(int, input().split())) for _ in range(0,n)]
-
+    
+    # Read node information
     tree = []
-
-    for line in lines:
-        node = Node(line[0])
-        node.left = line[1]
-        node.right = line[2]
+    for _ in range(n):
+        key, left, right = map(int, input().split())
+        node = Node(key)
+        node.left = left
+        node.right = right
         tree.append(node)
     
-
-    in_ord = in_order_traversal(tree, tree[0], res=[])
-    print(in_ord)
-    pre_ord = pre_order_traversal(tree, tree[0], res=[])
-    print(pre_ord)
-    post_ord = post_order_traversal(tree, tree[0], res=[])
-    print(post_ord)
+    # Perform and print the three traversals
+    print(' '.join(map(str, in_order_traversal(tree, tree[0], []))))
+    print(' '.join(map(str, pre_order_traversal(tree, tree[0], []))))
+    print(' '.join(map(str, post_order_traversal(tree, tree[0], []))))
